eed71d53ac8be8491c361eceb4f0e09d54a957f0
dejavuyj <xiaoyaoj_0346@qq.com>
2020-03-28 22:43:04 +0800

    TrieTree增加getWordsWithPrefix,返回以prefix开头的words

--- leetcode/algorithm/trie/Code208_implement_trie_prefix_tree.java
+++ leetcode/algorithm/trie/Code208_implement_trie_prefix_tree.java
@@ -1,27 +1,32 @@
 package leetcode.algorithm.trie;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
 public class Code208_implement_trie_prefix_tree {
 
 	static final int ALPHABET_SIZE = 26;
 	TrieNode root;
 
 	class TrieNode {
+		char data;
 		TrieNode[] children = new TrieNode[ALPHABET_SIZE];
 		public boolean isEndOfWord = false;
 
-		// String item = "";
-		TrieNode() {
+		String word;
 
+		TrieNode(char c) {
+			this.data = c;
 		}
 	}
-	// Trie[] children = new Trie[ALPHABET_SIZE];
 
 	/** Initialize your data structure here. */
 	public Code208_implement_trie_prefix_tree() {
-//		for (int i = 0; i < ALPHABET_SIZE; i++) {
-//			children[i] = null;
-//		}
-		root = new TrieNode();
+		root = new TrieNode(' ');
 	}
 
 	/** Inserts a word into the trie. */
@@ -34,12 +39,13 @@ public class Code208_implement_trie_prefix_tree {
 		for (char c : ca) {
 			index = c - 'a';
 			if (ptr[index] == null) {
-				ptr[index] = new TrieNode();
+				ptr[index] = new TrieNode(c);
 			}
 			lastTrie = ptr[index];
 			ptr = ptr[index].children;
 		}
 		lastTrie.isEndOfWord = true;
+		lastTrie.word = word;
 	}
 
 	/** Returns if the word is in the trie. */
@@ -79,13 +85,57 @@ public class Code208_implement_trie_prefix_tree {
 		return true;
 	}
 
+	// 返回以prefix开头的words
+	public List<String> getWordsWithPrefix(String prefix) {
+		List<String> l = new ArrayList<String>();
+		TrieNode[] ptr = root.children;
+		int index = 0;
+		for (int i = 0; i < prefix.length(); i++) {
+			index = prefix.charAt(i) - 'a';
+			if (ptr[index] == null) {
+				return l;
+			}
+			ptr = ptr[index].children;
+		}
+		Queue<TrieNode> queue = new LinkedList<TrieNode>();
+		processChildren(l, ptr, queue);
+		while (!queue.isEmpty()) {
+			TrieNode node = queue.poll();
+			ptr = node.children;
+			processChildren(l, ptr, queue);
+		}
+		return l;
+	}
+
+	private void processChildren(List<String> l, TrieNode[] ptr, Queue<TrieNode> queue) {
+		for (TrieNode node : ptr) {
+			if (node != null) {
+				queue.add(node);
+				if (node.isEndOfWord) {
+					l.add(node.word);
+				}
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		Code208_implement_trie_prefix_tree trie = new Code208_implement_trie_prefix_tree();
+		trie.insert("al");
+		trie.insert("also");
 		trie.insert("apple");
-		System.out.println(trie.search("apple")); // 返回 true
-		System.out.println(trie.search("app")); // 返回 false
-		System.out.println(trie.startsWith("app")); // 返回 true
+		trie.insert("apk");
 		trie.insert("app");
-		System.out.println(trie.search("app")); // 返回 true
+		trie.insert("delta");
+		trie.insert("program");
+		trie.insert("see");
+		// System.out.println(trie.search("apple")); // 返回 true
+		// System.out.println(trie.search("app")); // 返回 false
+		// System.out.println(trie.startsWith("app")); // 返回 true
+		// trie.insert("app");
+		// System.out.println(trie.search("app")); // 返回 true
+		List<String> l = trie.getWordsWithPrefix("a");
+		for (String s : l) {
+			System.out.println(s);
+		}
 	}
 }
