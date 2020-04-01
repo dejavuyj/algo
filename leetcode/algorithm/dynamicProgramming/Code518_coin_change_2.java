31004cc5930bc5d2839160a886d4d1ce6dfd2dc7
dejavuyj <xiaoyaoj_0346@qq.com>
2020-04-01 23:04:55 +0800

    使用组合数而非排列数解零钱兑换问题

---
+++ leetcode/algorithm/dynamicProgramming/Code518_coin_change_2.java
@@ -0,0 +1,29 @@
+package leetcode.algorithm.dynamicProgramming;
+
+public class Code518_coin_change_2 {
+
+	// 使用组合数而非排列数
+	// https://leetcode-cn.com/problems/coin-change-2/solution/ling-qian-dui-huan-iihe-pa-lou-ti-wen-ti-dao-di-yo/
+	public int change(int amount, int[] coins) {
+		int[] dp = new int[amount + 1];
+		dp[0] = 1;
+		for (int coin : coins) {
+			for (int i = coin; i <= amount; i++) {
+				if (coin <= i) {
+					dp[i] += dp[i - coin];
+				}
+			}
+		}
+		return dp[amount];
+	}
+
+	public static void main(String[] args) {
+		Code518_coin_change_2 c = new Code518_coin_change_2();
+		int[] coins = { 5, 1, 2 };
+		int amount = 5;
+		// int[] coins = { 2 };
+		// int amount = 3;
+		int ans = c.change(amount, coins);
+		System.out.println(ans);
+	}
+}
