57ee622795cd99034e9ebb8946d87ed19af24a9a
dejavuyj <xiaoyaoj_0346@qq.com>
2020-04-04 18:22:18 +0800

    动态规划解决分割等和子集问题

--- leetcode/algorithm/dynamicProgramming/Code416_partition_equal_subset_sum.java
+++ leetcode/algorithm/dynamicProgramming/Code416_partition_equal_subset_sum.java
@@ -1,6 +1,6 @@
 package leetcode.algorithm.dynamicProgramming;
 
-//等价于0-1背包问题
+// 等价于0-1背包问题
 public class Code416_partition_equal_subset_sum {
 
 	boolean ret = false;
@@ -11,7 +11,7 @@ public class Code416_partition_equal_subset_sum {
 		for (int i : nums) {
 			sum += i;
 		}
-		if(sum %2 != 0) {
+		if (sum % 2 != 0) {
 			return false;
 		}
 		int half = sum >> 1;
@@ -26,7 +26,7 @@ public class Code416_partition_equal_subset_sum {
 			ret = true;
 			return;
 		}
-		if(i == nums.length) {
+		if (i == nums.length) {
 			return;
 		}
 		backTrack(nums, i + 1, cw, target);
@@ -37,12 +37,37 @@ public class Code416_partition_equal_subset_sum {
 
 	// 动态规划
 	public boolean canPartition2(int[] nums) {
-		return false;
+		int sum = 0;
+		for (int i : nums) {
+			sum += i;
+		}
+		if ((sum & 1) == 1) {
+			return false;
+		}
+		int half = sum >> 1;
+
+		int n = nums.length;
+		boolean[] states = new boolean[half + 1];
+		states[0] = true;
+		if (nums[0] <= half) {
+			states[nums[0]] = true;
+		}
+		for (int i = 1; i < n; i++) {
+			for (int j = half - nums[i]; j >= 0; j--) {
+				if (states[half]) {
+					return true;
+				}
+				if (states[j] == true) {
+					states[j + nums[i]] = true;
+				}
+			}
+		}
+		return states[half];
 	}
 
 	public static void main(String[] args) {
 		Code416_partition_equal_subset_sum c = new Code416_partition_equal_subset_sum();
-		int[] a = { 1, 5, 11, 5 };
-		System.out.println(c.canPartition(a));
+		int[] a = { 1, 2, 5 };
+		System.out.println(c.canPartition2(a));
 	}
 }
