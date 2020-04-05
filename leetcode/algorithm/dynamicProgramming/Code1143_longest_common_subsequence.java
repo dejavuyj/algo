1edb2866f463f1be0a7839bae84c980e66345dae
dejavuyj <xiaoyaoj_0346@qq.com>
2020-04-05 21:31:35 +0800

    动态规划解决最长公共子序列问题,降低空间复杂度

--- leetcode/algorithm/dynamicProgramming/Code1143_longest_common_subsequence.java
+++ leetcode/algorithm/dynamicProgramming/Code1143_longest_common_subsequence.java
@@ -2,7 +2,8 @@ package leetcode.algorithm.dynamicProgramming;
 
 public class Code1143_longest_common_subsequence {
 
-    public int longestCommonSubsequence(String text1, String text2) {
+	// 空间复杂度(m*n)
+	public int longestCommonSubsequence(String text1, String text2) {
 		int m = text1.length();
 		int n = text2.length();
 		if (m * n == 0) {
@@ -21,10 +22,44 @@ public class Code1143_longest_common_subsequence {
 		return dp[m][n];
 	}
 
+	// 空间复杂度min(m,n)
+	public int longestCommonSubsequence2(String text1, String text2) {
+		int m = text1.length();
+		int n = text2.length();
+		if (m == 0 || n == 0) {
+			return 0;
+		}
+		String rowText;
+		String colText;
+		if (m > n) {
+			rowText = text1;
+			colText = text2;
+		} else {
+			rowText = text2;
+			colText = text1;
+		}
+		int[] dp = new int[colText.length() + 1];
+		for (int i = 1; i < rowText.length() + 1; i++) {
+			int tmp = 0;
+			for (int j = 1; j < colText.length() + 1; j++) {
+				int prev = tmp; // 对应上一行前一列的值,等价于二维数组中的dp[i-1][j-1]
+				tmp = dp[j];
+				if (colText.charAt(j - 1) == rowText.charAt(i - 1)) {
+					dp[j] = prev + 1;
+				} else {
+					// dp[j]  为上一行当前列, 相当于二维数组中的dp[i-1][j]
+					// dp[j-1]为当前行前一列, 相当于二维数组中的dp[i][j-1]
+					dp[j] = Math.max(dp[j], dp[j - 1]);
+				}
+			}
+		}
+		return dp[colText.length()];
+	}
+
 	public static void main(String[] args) {
 		Code1143_longest_common_subsequence c = new Code1143_longest_common_subsequence();
-		// int ans = c.longestCommonSubsequence("abcde", "ace");
-		int ans = c.longestCommonSubsequence("abc", "def");
+		int ans = c.longestCommonSubsequence("bsbininm", "jmjkbkjkv");
+		// int ans = c.longestCommonSubsequence("abc", "def");
 		System.out.println(ans);
 	}
 }
