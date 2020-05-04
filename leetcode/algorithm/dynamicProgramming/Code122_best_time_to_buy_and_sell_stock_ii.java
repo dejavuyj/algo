d8431cb36c319dc3438af475610e47f010733efc
dejavuyj <xiaoyaoj_0346@qq.com>
2020-05-04 08:48:29 +0800

    leetcode122 增加一种动态规划解法

--- leetcode/algorithm/dynamicProgramming/Code122_best_time_to_buy_and_sell_stock_ii.java
+++ leetcode/algorithm/dynamicProgramming/Code122_best_time_to_buy_and_sell_stock_ii.java
@@ -45,6 +45,23 @@ public class Code122_best_time_to_buy_and_sell_stock_ii {
 			return 0;
 		}
 
+		int[][] dp = new int[prices.length][2];
+		dp[0][0] = 0;
+		dp[0][1] = -prices[0];
+
+		for (int i = 1; i < prices.length; i++) {
+			dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+			dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+		}
+		return dp[prices.length - 1][0];
+	}
+
+	// ¶¯Ì¬¹æ»®
+	public static int maxProfit4(int[] prices) {
+		if (prices == null) {
+			return 0;
+		}
+
 		int dp_i_0 = 0;
 		int dp_i_1 = Integer.MIN_VALUE;
 
