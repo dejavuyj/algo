339d878dadd222c9d8289b7befb345779c40922d
dejavuyj <xiaoyaoj_0346@qq.com>
2020-07-15 22:32:35 +0800

    code89. 格雷编码

---
+++ leetcode/algorithm/dynamicProgramming/Code89_gray_code.java
@@ -0,0 +1,27 @@
+package leetcode.algorithm.dynamicProgramming;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Code89_gray_code {
+
+	public List<Integer> grayCode(int n) {
+		List<Integer> ansList = new ArrayList<Integer>(1 << n);
+		ansList.add(0);
+		for (int i = 0; i < n; i++) {
+			int add = 1 << i;
+			for (int j = ansList.size() - 1; j >= 0; j--) {
+				ansList.add(ansList.get(j) + add);
+			}
+		}
+		return ansList;
+	}
+
+	public static void main(String[] args) {
+		Code89_gray_code c = new Code89_gray_code();
+		List<Integer> ansList = c.grayCode(3);
+		for (int i : ansList) {
+			System.out.print(i + ", ");
+		}
+	}
+}
