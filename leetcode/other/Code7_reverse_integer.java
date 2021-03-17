74ac64fe0f41320656a7161035387aa4b8af6165
dejavuyj <dejavuyj9@gmail.com>
2021-03-17 19:22:14 +0800

    reverse3

--- leetcode/other/Code7_reverse_integer.java
+++ leetcode/other/Code7_reverse_integer.java
@@ -62,10 +62,30 @@ public class Code7_reverse_integer {
         return ret;
     }
 
+    public int reverse3(int x) {
+        int ret = 0;
+        boolean flag = false;
+        if (x < 0) {
+            flag = true;
+            x = x * -1;
+        }
+
+        while (x != 0) {
+            int digit = x%10;
+            if (ret > (Integer.MAX_VALUE - digit) / 10) {
+                return 0;
+            }
+            ret = ret*10 + digit;
+            x /= 10;
+        }
+        return flag ? ret * -1 : ret;
+    }
+
     public static void main(String[] args) {
+        int x2 = 12%10;
         Code7_reverse_integer c = new Code7_reverse_integer();
-        int x = 1534236469;
-        int y = c.reverse(x);
+        int x = 1231121212;
+        int y = c.reverse3(x);
         System.out.println(y);
     }
 }
