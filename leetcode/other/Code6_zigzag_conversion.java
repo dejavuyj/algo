b3747213f72829a5c0633cfef3d04bd599c2c289
袁俊 <yuanjun.yuan@alibaba-inc.com>
2021-04-27 23:11:35 +0800

    convert2

--- leetcode/other/Code6_zigzag_conversion.java
+++ leetcode/other/Code6_zigzag_conversion.java
@@ -40,11 +40,37 @@ public class Code6_zigzag_conversion {
         return sb.toString();
     }
 
+    public String convert2(String s, int numRows) {
+        if (numRows == 1)
+            return s;
+
+        int zNum = 2 * numRows - 2;
+        int sLen = s.length();
+        int zCnt = (sLen % zNum == 0 ? sLen / zNum : sLen / zNum + 1);
+        StringBuilder sb = new StringBuilder();
+
+        for (int curRow=0; curRow<numRows; curRow++) {
+            for (int i = 0; i < zCnt; i++) {
+                int curIndex = i * zNum + curRow;
+                if (curIndex < sLen) {
+                    sb.append(s.charAt(curIndex));
+                }
+                if (curRow != 0 && curRow != numRows - 1) {
+                    curIndex += zNum - 2 * curRow;
+                    if (curIndex < sLen) {
+                        sb.append(s.charAt(curIndex));
+                    }
+                }
+            }
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
-        String str = "AB";
-        int numRows = 1;
+        String str = "PAYPALISHIRING";
+        int numRows = 3;
         Code6_zigzag_conversion c = new Code6_zigzag_conversion();
-        str = c.convert(str, numRows);
+        str = c.convert2(str, numRows);
         System.out.print(str);
     }
 }
