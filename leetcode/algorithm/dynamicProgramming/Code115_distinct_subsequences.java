8ebb63c2d027a8ba4c23c0202ede95bb408f78f3
dejavuyj <xiaoyaoj_0346@qq.com>
2022-09-11 21:37:28 +0800

    Code115_distinct_subsequences

---
+++ leetcode/algorithm/dynamicProgramming/Code115_distinct_subsequences.java
@@ -0,0 +1,35 @@
+package leetcode.algorithm.dynamicProgramming;
+
+public class Code115_distinct_subsequences {
+
+    public int numDistinct(String s, String t) {
+        int m = s.length(), n = t.length();
+        if (m < n) {
+            return 0;
+        }
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            dp[i][n] = 1;
+        }
+        for (int i = m - 1; i >= 0; i--) {
+            char sChar = s.charAt(i);
+            for (int j = n - 1; j >= 0; j--) {
+                char tChar = t.charAt(j);
+                if (sChar == tChar) {
+                    dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j];
+                } else {
+                    dp[i][j] = dp[i + 1][j];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+
+    public static void main(String[] args) {
+        Code115_distinct_subsequences c = new Code115_distinct_subsequences();
+        String s = "babgbag";
+        String t = "bag";
+        int ans = c.numDistinct(s, t);
+        System.out.println(ans);
+    }
+}
