63da4bad260e5ae74e49b014a7791968dfad4b18
dejavuyj <xiaoyaoj_0346@qq.com>
2023-11-19 11:00:37 +0800

    Code207_course_schedule

---
+++ leetcode/dataStructure/graph/Code207_course_schedule.java
@@ -0,0 +1,52 @@
+package leetcode.dataStructure.graph;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Code207_course_schedule {
+
+    List<List<Integer>> edges;
+    int[] indeg;
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        edges = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            edges.add(new ArrayList<>());
+        }
+        indeg = new int[numCourses];
+        for (int[] info : prerequisites) {
+            edges.get(info[1]).add(info[0]);
+            ++indeg[info[0]];
+        }
+
+        Queue<Integer> queue = new LinkedList<Integer>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indeg[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        int visited = 0;
+        while (!queue.isEmpty()) {
+            ++visited;
+            int u = queue.poll();
+            for (int v : edges.get(u)) {
+                --indeg[v];
+                if (indeg[v] == 0) {
+                    queue.offer(v);
+                }
+            }
+        }
+
+        return visited == numCourses;
+    }
+
+    public static void main(String[] args) {
+        Code207_course_schedule c = new Code207_course_schedule();
+        int numCourses = 2;
+        int[][] prerequisites = {{1, 0}};
+        System.out.println(c.canFinish(numCourses, prerequisites));
+    }
+}
