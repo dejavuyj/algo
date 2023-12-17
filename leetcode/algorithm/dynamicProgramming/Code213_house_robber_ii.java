b590818bae65eced89fd5b9393f435cd55450647
dejavuyj <xiaoyaoj_0346@qq.com>
2023-12-17 08:38:32 +0800

    Code213_house_robber_ii

---
+++ leetcode/algorithm/dynamicProgramming/Code213_house_robber_ii.java
@@ -0,0 +1,31 @@
+package leetcode.algorithm.dynamicProgramming;
+
+public class Code213_house_robber_ii {
+
+    public int rob(int[] nums) {
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        } else if (length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+
+        return Math.max(robRange(nums, 0, length - 2), robRange(nums, 1, length - 1));
+    }
+
+    public int robRange(int[] nums, int start, int end) {
+        int first = nums[start], second = Math.max(nums[start], nums[start + 1]);
+        for (int i = start + 2; i <= end; i++) {
+            int temp = second;
+            second = Math.max(first + nums[i], second);
+            first = temp;
+        }
+        return second;
+    }
+
+    public static void main(String[] args) {
+        Code213_house_robber_ii c = new Code213_house_robber_ii();
+        int[] nums = {1, 2, 9, 9};
+        System.out.println(c.rob(nums));
+    }
+}
