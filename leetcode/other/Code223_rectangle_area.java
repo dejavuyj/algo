2ae1c08deeb9e45abccac21ba3bff58ac8a112e9
dejavuyj <xiaoyaoj_0346@qq.com>
2024-02-18 09:17:56 +0800

    Code223_rectangle_area

---
+++ leetcode/other/Code223_rectangle_area.java
@@ -0,0 +1,19 @@
+package leetcode.other;
+
+public class Code223_rectangle_area {
+
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int area1 = (ax2 - ax1) * (ay2 - ay1), area2 = (bx2 - bx1) * (by2 - by1);
+        int overlapWidth = Math.min(ax2, bx2) - Math.max(ax1, bx1);
+        int overlapHeight = Math.min(ay2, by2) - Math.max(ay1, by1);
+        int overlapArea = Math.max(overlapWidth, 0) * Math.max(overlapHeight, 0);
+        return area1 + area2 - overlapArea;
+    }
+
+    public static void main(String[] args) {
+        Code223_rectangle_area c = new Code223_rectangle_area();
+        int ax1 = -3, ay1 = 0, ax2 = 3, ay2 = 4, bx1 = 0, by1 = -1, bx2 = 9, by2 = 2;
+        int ret = c.computeArea(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2);
+        System.out.print(ret);
+    }
+}
