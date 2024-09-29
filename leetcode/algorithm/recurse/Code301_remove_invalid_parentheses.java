d3f72891f6e16901fc931a2163dec05f69895265
dejavuyj <xiaoyaoj_0346@qq.com>
2024-09-29 09:15:23 +0800

    Code301_remove_invalid_parentheses

---
+++ leetcode/algorithm/recurse/Code301_remove_invalid_parentheses.java
@@ -0,0 +1,75 @@
+package leetcode.algorithm.recurse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Code301_remove_invalid_parentheses {
+
+    List<String> res = new ArrayList<>();
+
+    public List<String> removeInvalidParentheses(String s) {
+        int lremove = 0;
+        int rremove = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                lremove++;
+            } else if (s.charAt(i) == ')') {
+                if (lremove == 0) {
+                    rremove++;
+                } else {
+                    lremove--;
+                }
+            }
+        }
+        helper(s, 0, lremove, rremove);
+
+        return res;
+    }
+
+    private void helper(String str, int start, int lremove, int rremove) {
+        if (lremove == 0 && rremove == 0) {
+            if (isValid(str)) {
+                res.add(str);
+            }
+            return;
+        }
+
+        for (int i = start; i < str.length(); i++) {
+            if (i != start && str.charAt(i) == str.charAt(i - 1)) {
+                continue;
+            }
+            if (lremove + rremove > str.length() - i) {
+                return;
+            }
+            if (lremove > 0 && str.charAt(i) == '(') {
+                helper(str.substring(0, i) + str.substring(i + 1), i, lremove - 1, rremove);
+            }
+            if (rremove > 0 && str.charAt(i) == ')') {
+                helper(str.substring(0, i) + str.substring(i + 1), i, lremove, rremove - 1);
+            }
+        }
+    }
+
+    private boolean isValid(String str) {
+        int cnt = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') {
+                cnt++;
+            } else if (str.charAt(i) == ')') {
+                cnt--;
+                if (cnt < 0) {
+                    return false;
+                }
+            }
+        }
+        return cnt == 0;
+    }
+
+    public static void main(String[] majorityElement2) {
+        Code301_remove_invalid_parentheses c = new Code301_remove_invalid_parentheses();
+        String s = "()())()";
+        List<String> ansList = c.removeInvalidParentheses(s);
+        ansList.forEach(System.out::println);
+    }
+}
