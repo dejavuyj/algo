d5dd668d34b74eb5e08da63c4e9854f337d7ba31
dejavuyj <xiaoyaoj_0346@qq.com>
2024-11-10 08:36:07 +0800

    Code312_burst_balloons

---
+++ leetcode/algorithm/dynamicProgramming/Code312_burst_balloons.java
@@ -0,0 +1,30 @@
+package leetcode.algorithm.dynamicProgramming;
+
+public class Code312_burst_balloons {
+
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[][] rec = new int[n + 2][n + 2];
+        int[] val = new int[n + 2];
+        val[0] = val[n + 1] = 1;
+        for (int i = 1; i <= n; i++) {
+            val[i] = nums[i - 1];
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 2; j <= n + 1; j++) {
+                for (int k = i + 1; k < j; k++) {
+                    int sum = val[i] * val[k] * val[j];
+                    sum += rec[i][k] + rec[k][j];
+                    rec[i][j] = Math.max(rec[i][j], sum);
+                }
+            }
+        }
+        return rec[0][n + 1];
+    }
+
+    public static void main(String[] args) {
+        Code312_burst_balloons c = new Code312_burst_balloons();
+        int[] nums = {3, 1, 5, 8};
+        System.out.println(c.maxCoins(nums));
+    }
+}
