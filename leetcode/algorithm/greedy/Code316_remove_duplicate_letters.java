39003ffc3536b1733d9bc2f5c0d79bccc86cbbf9
dejavuyj <xiaoyaoj_0346@qq.com>
2024-12-01 10:12:58 +0800

    Code316_remove_duplicate_letters

---
+++ leetcode/algorithm/greedy/Code316_remove_duplicate_letters.java
@@ -0,0 +1,37 @@
+package leetcode.algorithm.greedy;
+
+public class Code316_remove_duplicate_letters {
+
+    public String removeDuplicateLetters(String s) {
+        boolean[] vis = new boolean[26];
+        int[] num = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            num[s.charAt(i) - 'a']++;
+        }
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!vis[ch - 'a']) {
+                while (sb.length() > 0 && sb.charAt(sb.length() - 1) > ch) {
+                    if (num[sb.charAt(sb.length() - 1) - 'a'] > 0) {
+                        vis[sb.charAt(sb.length() - 1) - 'a'] = false;
+                        sb.deleteCharAt(sb.length() - 1);
+                    } else {
+                        break;
+                    }
+                }
+                vis[ch - 'a'] = true;
+                sb.append(ch);
+            }
+            num[ch - 'a'] -= 1;
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Code316_remove_duplicate_letters c = new Code316_remove_duplicate_letters();
+        String s = "bcabc";
+        String ret = c.removeDuplicateLetters(s);
+        System.out.print(ret);
+    }
+}
